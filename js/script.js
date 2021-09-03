16ea09d37733cf761de55fc62b0a9fd682c658a6
Roney11 <alexsilva270995@gmail.com>
2021-09-02 21:41:30 -0300

    adicionando form para agendar sessão de fotos

--- js/script.js
+++ js/script.js
@@ -74,3 +74,52 @@ function slideImgBody() {
     }
 }
 slideImgBody()
+
+const btn = document.querySelectorAll ('.planos button')
+const adquirir = document.querySelector('.js-btn1')
+const adquirir2 = document.querySelector('.js-btn2')
+const adquirir3 = document.querySelector('.js-btn3')
+var mobal = document.querySelector('.agendar')
+
+//pegando as classes dos tres botoes
+adquirir.addEventListener('click', abrirMobal)
+adquirir2.addEventListener('click', abrirMobal)
+adquirir3.addEventListener('click', abrirMobal)
+
+//fazendo um foreach para remover a classe adquiri
+//caso exista e adicionando a mesma ao clique
+//carregando o style do mobal
+function abrirMobal(item) {
+    btn.forEach ((item) => {
+        item.classList.remove ('adquirir')
+    })
+
+    item.currentTarget.classList.add ('adquirir')
+    mobal.style.marginLeft = '0'
+    mobal.style.transition ='0.8s'
+}
+
+//pegando a classe do botao enviar e adicionando um clique
+//no mesmo
+const enviar = document.querySelector('.enviar')
+enviar.addEventListener('click', fecharMobal)
+
+//funcao para 'salvar' os dados e fechar o modal ao 
+//click
+//obs: como o intuito não tras back-end, não foi
+//utilizado php para fazer as verificações e validar
+//o formulario 
+function salvarMobal() {
+    mobal.style.marginLeft = '-100%'
+    mobal.style.transition ='0.8s'
+}
+
+//pegando a classe para cancelar o form e 
+//fechar o modal
+const cancelar = document.querySelector('.cancelar')
+cancelar.addEventListener('click', salvarMobal)
+
+function fecharMobal() {
+    mobal.style.marginLeft = '-100%'
+    mobal.style.transition ='0.8s'
+}
